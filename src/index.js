7d35b5b645ee03e8ea1f03a21d32f71042c7ce1d
krenvp <karenperarnau@gmail.com>
2023-07-08 13:21:07 -0400

    Creating the page structure

---
+++ src/index.js
@@ -0,0 +1,26 @@
+/**
+ * Home template
+ * 
+ *   <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="../src/assets/icons/favicon-32x32.png"
+    />
+    <title>Interactive comments section</title>
+  </head>
+  <body>
+    <main id="app"></main>
+
+    <footer>
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+        >Frontend Mentor</a
+      >. Coded by
+      <a href="https://github.com/vkaren" target="_blank">Karen Varela</a>.
+    </footer>
+  </body>
+ */
