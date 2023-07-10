cb289809d9c47cf1bd01fb0f1150cf48f3362764
krenvp <karenperarnau@gmail.com>
2023-07-10 15:23:14 -0400

    Creating home page

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "@components/Layout";
+import SignIn from "@components/SignIn";
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <SignIn />
+    </Layout>
+  );
+};
+
+export default HomePage;
