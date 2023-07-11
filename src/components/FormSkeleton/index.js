9a08e2c6fe30d211e3d8b2c32bcdabeb049802b7
krenvp <karenperarnau@gmail.com>
2023-07-11 14:59:53 -0400

    Creating FormSkeleton component

---
+++ src/components/FormSkeleton/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const FormSkeleton = () => (
+  <section className={styles["form-skeleton"]}></section>
+);
+
+export default FormSkeleton;
