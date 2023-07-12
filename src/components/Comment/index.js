0721625a7ba244bb1c217cb2deb455735b304d73
krenvp <karenperarnau@gmail.com>
2023-07-12 15:50:03 -0400

    Adding active styles

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import iconPlus from "@icons/icon-plus.svg";
-import iconMinus from "@icons/icon-minus.svg";
-import iconReply from "@icons/icon-reply.svg";
-import iconDelete from "@icons/icon-delete.svg";
-import iconEdit from "@icons/icon-edit.svg";
+import PlusIcon from "@icons/icon-plus.svg";
+import MinusIcon from "@icons/icon-minus.svg";
+import ReplyIcon from "@icons/icon-reply.svg";
+import DeleteIcon from "@icons/icon-delete.svg";
+import EditIcon from "@icons/icon-edit.svg";
 import styles from "./styles.module.css";
 
 const Comment = ({
@@ -21,38 +21,43 @@ const Comment = ({
       <div className={styles["comment_profile-photo"]}>
         <Image src={user.image.png} width={25} height={25} />
       </div>
+
       <span className={styles["comment_username"]}>{user.username}</span>
+
       {currentUser === user.username && (
         <span className={styles["comment_current-user"]}>you</span>
       )}
+
       <span className={styles["comment_time"]}>{createdAt}</span>
     </header>
 
     {currentUser === user.username ? (
       <>
         <button className={styles["comment_delete-btn"]}>
-          <Image src={iconDelete} width={10} />
+          <DeleteIcon />
           <span>Delete</span>
         </button>
         <button className={styles["comment_edit-btn"]}>
-          <Image src={iconEdit} width={10} />
+          <EditIcon />
           <span>Edit</span>
         </button>
       </>
     ) : (
       <button className={styles["comment_reply-btn"]}>
-        <Image src={iconReply} width={10} />
+        <ReplyIcon />
         <span>Reply</span>
       </button>
     )}
 
     <div className={styles["comment_like"]}>
       <button className={styles["comment_like-up"]}>
-        <Image src={iconPlus} width={10} />
+        <PlusIcon />
       </button>
+
       <span className={styles["comment_likes"]}>{score}</span>
+
       <button className={styles["comment_like-down"]}>
-        <Image src={iconMinus} width={10} />
+        <MinusIcon />
       </button>
     </div>
 
