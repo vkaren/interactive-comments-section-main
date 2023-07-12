3852c4e0d4b121fe0e4551791c8ed6e32575495b
krenvp <karenperarnau@gmail.com>
2023-07-12 15:17:57 -0400

    Editing CommentThread component

--- src/components/CommentThread/index.js
+++ src/components/CommentThread/index.js
@@ -1,36 +1,24 @@
 import React from "react";
-import { withRouter } from "next/router";
 import CommentContainer from "@components/CommentContainer";
-import data from "@data/data.json";
 import styles from "./styles.module.css";
 
-class CommentThread extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comments: data.comments,
-      currentUser: this.props.user,
-    };
-  }
+const CommentThread = ({ comments, currentUser }) => {
+  return (
+    <section className={styles["comments_section"]}>
+      {comments.map((comment) => (
+        <CommentContainer
+          key={comment.id}
+          id={comment.id}
+          user={comment.user}
+          currentUser={currentUser}
+          content={comment.content}
+          createdAt={comment.createdAt}
+          score={comment.score}
+          replies={comment.replies}
+        />
+      ))}
+    </section>
+  );
+};
 
-  render() {
-    return (
-      <section className={styles["comments_section"]}>
-        {this.state.comments.map((comment) => (
-          <CommentContainer
-            key={comment.id}
-            id={comment.id}
-            user={comment.user}
-            currentUser={this.state.currentUser}
-            content={comment.content}
-            createdAt={comment.createdAt}
-            score={comment.score}
-            replies={comment.replies}
-          />
-        ))}
-      </section>
-    );
-  }
-}
-
-export default withRouter(CommentThread);
+export default CommentThread;
