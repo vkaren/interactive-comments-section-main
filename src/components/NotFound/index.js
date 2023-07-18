b483ff800a9c07b7603e4a67d45da2dc4d98a745
krenvp <karenperarnau@gmail.com>
2023-07-18 19:34:35 -0400

    Adding 404 page

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import iconNotFound from "@icons/icon-404.png";
+import styles from "./styles.module.css";
+
+const NotFound = () => {
+  return (
+    <section className={styles["not-found"]}>
+      <Image src={iconNotFound} width={50} height={50} />
+      <h1>Page Not Found</h1>
+    </section>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import Layout from "@components/Layout";
+import NotFound from "@components/NotFound";
+
+const Custom404 = () => {
+  return (
+    <Layout>
+      <NotFound />
+    </Layout>
+  );
+};
+
+export default Custom404;
