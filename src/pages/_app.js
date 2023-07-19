7e998d7bc7457a5da72fb0c78d02c03925865e9e
krenvp <karenperarnau@gmail.com>
2023-07-19 09:59:56 -0400

    Installing sharp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,5 @@
 import "../global.css";
 
-export function reportWebVitals(metric) {
-  console.log(metric);
-}
-
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
